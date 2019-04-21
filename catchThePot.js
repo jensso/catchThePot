9a1e7876f1459ab63a50ad1201a222da5e967c96
jens soltwedel <jens.soltwedel@hotmail.com>
2019-04-21 20:25:42 +0200

    appearance optimized, timeout for alert-msg

--- catchThePot.js
+++ catchThePot.js
@@ -1,37 +1,41 @@
 const field = document.getElementById("field");
 const pot = document.getElementById("pot");
+const alert = document.getElementById('alert');
+
 
 const hideThePot = (()=> {
-  let potPosX = Math.round(Math.random()*80)+"%";
-  let potPosY = Math.round(Math.random()*95)+"%";
+  let potPosX = Math.round(Math.random()*60);
+  let potPosY = Math.round(Math.random()*90);
 
-  pot.style.top = potPosX;
-  pot.style.left = potPosY;
-  console.error(potPosX);
-  console.error(potPosY);
+  pot.style.top = potPosX +"%";
+  pot.style.left = potPosY +"%";
 })();
 let fieldX;
 let fieldY;
 let potX;
 let potY;
+
 field.addEventListener('click',(ev)=> {
-  let alert = document.getElementById('alert');
-console.log(alert);
   fieldX = ev.clientX;
   fieldY = ev.clientY;
   console.log(fieldX);
+
   if (potX === fieldX && potY === fieldY) {
-    alert.innerText = 'Gotcha';
-    pot.style.transition = 'opacity 3s linear';
+    alert.innerText = 'Got it !';
+    alert.style.background = 'green';
+    pot.style.transition = 'opacity 2s linear';
     pot.style.opacity = '1';
   }
   else {
     alert.innerText = "missed it";
+    alert.style.background = 'salmon';
+
+    setTimeout(()=> {
+    alert.innerText = "";
+  },2000)
   }
 })
 pot.addEventListener('click',(ev)=> {
   potX = ev.clientX;
   potY = ev.clientY;
-  console.log(potX);
-
 })
